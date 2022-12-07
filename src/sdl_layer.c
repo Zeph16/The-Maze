953e583344cba34af9bea245a7114a48a46f95cc
Yordi-Dereje <yordadereje@gmail.com>
2022-12-07 14:03:46 -0800

    betty

--- src/sdl_layer.c
+++ src/sdl_layer.c
@@ -4,41 +4,38 @@
 SDL_Renderer *renderer;
 SDL_Window *window;
 
-void initsdl()
+void initsdl(void)
 {
-    SDL_Init(SDL_INIT_VIDEO);
-    window = SDL_CreateWindow("SDL Experiment", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, width, height, SDL_WINDOW_OPENGL);
-    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+	SDL_Init(SDL_INIT_VIDEO);
+	window = SDL_CreateWindow("SDL Experiment", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, width, height, SDL_WINDOW_OPENGL);
+	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 }
 
-void handleEvents()
-{
-    SDL_Event event;
-    const Uint8 *keyStates = SDL_GetKeyboardState(NULL);
-    while (SDL_PollEvent(&event))
-    {
-        if (event.type == SDL_QUIT)
-        {
-            running = false;
-        }
-    }
-    movePlayer(keyStates);
+void handleEvents(void)
+{	SDL_Event event;
+	const Uint8 *keyStates = SDL_GetKeyboardState(NULL);
+	while (SDL_PollEvent(&event))
+	{
+		if (event.type == SDL_QUIT)
+			running = false;
+	}
+	movePlayer(keyStates);
 }
 
-void clearScreen()
+void clearScreen(void)
 {
-    SDL_SetRenderDrawColor(renderer, 30, 30, 30, 255);
-    SDL_RenderClear(renderer);
+	SDL_SetRenderDrawColor(renderer, 30, 30, 30, 255);
+	SDL_RenderClear(renderer);
 }
 
-void drawScreen()
+void drawScreen(void)
 {
-    SDL_RenderPresent(renderer);
+	SDL_RenderPresent(renderer);
 }
 
-void quitsdl()
+void quitsdl(void)
 {
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
 }
